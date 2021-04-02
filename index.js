9525855031f2c5606ff2c3496c34b9b0672d3015
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-02 11:59:33 +0600

    data inserted in database

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const port = 5000
+
 const password = 'iXYS6zJhx$8PUCp'
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://<username>:<password>@raufuprezensinc.hztjo.mongodb.net/burjAlArab?retryWrites=true&w=majority";
-
-
+const uri = "mongodb+srv://alarab:iXYS6zJhx$8PUCp@raufuprezensinc.hztjo.mongodb.net/burjAlArab?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
 client.connect(err => {
-    const collection = client.db("burjAlArab").collection("bookings");
+    const bookings = client.db("burjAlArab").collection("bookings");
     console.log("Database Connected Successfully!")
-    client.close();
+
+    app.post('/addBooking', (req, res) => {
+        const newBooking = req.body;
+        bookings.insertOne(newBooking)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+        console.log(newBooking)
+    })
 });
 
 
